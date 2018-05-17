997d40781690f54b8817d1126122a031c3e3b0a0
Fei Liu <liu.feiwood@gmail.com>
2018-05-17 16:16:57 +0200

    more work on media

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -31,7 +31,7 @@ const MediaCarousel = () => (
 )
 
 const Media = () => (
-  <div className="intro-section" style={{ paddingTop: '7rem', color: '#9B9B9B', position: 'relative' }}>
+  <div className="media-section" style={{ paddingTop: '10rem', color: '#9B9B9B', position: 'relative' }}>
     <Container>
       <Row>
         <Col>
