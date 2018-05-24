9dd8760d6fb981d1fe0abceaba85d0b8d4c2f1d2
Teng Qiu <tengqiu@kpmg.com>
2018-05-24 13:49:11 +0200

    Fix pic addresses

--- src/components/GalleryTeaser/pictures.js
+++ src/components/GalleryTeaser/pictures.js
@@ -1,36 +1,36 @@
 export default [
   {
-    src: '/images/teaser-01.jpeg',
+    src: '/images/media1.png',
     title: 'Preparing our ICO',
     description: 'Initial conversion with Google',
     reflink: ''
   },
   {
-    src: '/images/teaser-02.jpeg',
+    src: '/images/media2.png',
     title: 'Insides into Token ',
     description: 'How does Ares Tech Make Exchanging ERC721 Asset with ERC20 Token Possible',
     reflink: ''
   },
   {
-    src: '/images/teaser-03.jpeg',
+    src: '/images/media3.png',
     title: 'Explaining the vision',
     description: 'CEO Jack introduce Ares Tech',
     reflink: ''
   },
   {
-    src: '/images/teaser-04.jpeg',
+    src: '/images/media4.png',
     title: 'Meetup at the Berlin Office',
     description: '',
     reflink: ''
   },
   {
-    src: '/images/teaser-05.jpeg',
+    src: '/images/media5.png',
     title: 'Join Telegram - Get Candy',
     description: '',
     reflink: ''
   },
   {
-    src: '/images/teaser-06.jpeg',
+    src: '/images/media6.png',
     title: 'Blockchain Event',
     description: '',
     reflink: ''
