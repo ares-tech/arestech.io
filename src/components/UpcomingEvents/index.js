340ce6ca26bb2041eecfdeb1bf61c77b9dd61e9a
Fei Liu <liu.feiwood@gmail.com>
2018-06-06 18:16:24 +0200

    fix js lint

--- src/components/UpcomingEvents/index.js
+++ src/components/UpcomingEvents/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import moment from 'moment'
 import { FormattedMessage } from 'react-intl'
 import { Row, Col } from 'reactstrap'
 import './index.css'
@@ -12,10 +11,7 @@ const Events = ({ event }) => {
   }
 
   return (
-    <div
-      className="card-content event p-2"
-      style={{ ...style }}
-    >
+    <div className="card-content event p-2" style={{ ...style }}>
       <h3 className="event-header" style={{ color: event.headerColor }}>
         <FormattedMessage id={event.title} />
       </h3>
@@ -30,39 +26,22 @@ const Events = ({ event }) => {
   )
 }
 
-export default class UpcomingEvents extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: 0,
-    }
-  }
-
-  render() {
-    return (
-      <div className="upcomingEvents">
-        <div className="container">
-          <Row>
-            <Col>
-              <h1 className="display-2 headline" style={{ color: '#ff6427' }}>
-                <FormattedMessage id="upcomingEvents.title" />
-              </h1>
-              <p className="lead sub-headline" style={{ color: '#ff6427' }}>
-                <FormattedMessage id="upcomingEvents.subtitle" />
-              </p>
-            </Col>
-          </Row>
-        </div>
-        <div className="container">
-          <div className="cards" >
-            {events && events.map(event => {
-              return (
-                <Events event={event} key={event.title} />
-              )
-            })}
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+export default () => (
+  <div className="upcomingEvents">
+    <div className="container">
+      <Row>
+        <Col>
+          <h1 className="display-2 headline" style={{ color: '#ff6427' }}>
+            <FormattedMessage id="upcomingEvents.title" />
+          </h1>
+          <p className="lead sub-headline" style={{ color: '#ff6427' }}>
+            <FormattedMessage id="upcomingEvents.subtitle" />
+          </p>
+        </Col>
+      </Row>
+    </div>
+    <div className="container">
+      <div className="cards">{events && events.map(event => <Events event={event} key={event.title} />)}</div>
+    </div>
+  </div>
+)
