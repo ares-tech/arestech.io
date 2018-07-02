b4b793ace2429911b85cbc92ada4004912d4693e
Teng Qiu <tengqiu@kpmg.com>
2018-07-02 11:24:20 +0200

    Update latest news

--- src/components/GalleryTeaser/pictures.js
+++ src/components/GalleryTeaser/pictures.js
@@ -3,6 +3,30 @@ import { FormattedMessage } from 'react-intl'
 
 export default [
   {
+    src: '/images/media3.png',
+    title: <FormattedMessage id="update.media.2.title" />,
+    description: <FormattedMessage id="update.media.2.description" />,
+    link: 'https://medium.com/ares-tech/ares-tech-presented-on-global-blockchain-leadership-summit-hangzhou-e4108f8bdd81',
+  },
+  {
+    src: '/images/media4.png',
+    title: <FormattedMessage id="update.media.3.title" />,
+    description: <FormattedMessage id="update.media.3.description" />,
+    link: 'https://medium.com/ares-tech/ares-opening-technology-video-channel-554982b041b2',
+  },
+  {
+    src: '/images/media5.png',
+    title: <FormattedMessage id="update.media.4.title" />,
+    description: <FormattedMessage id="update.media.4.description" />,
+    link: 'https://medium.com/ares-tech/what-is-the-concept-behind-ares-techs-new-brand-design-410644786e6a',
+  },
+  {
+    src: '/images/media6.png',
+    title: <FormattedMessage id="update.media.5.title" />,
+    description: <FormattedMessage id="update.media.5.description" />,
+    link: 'https://medium.com/ares-tech/ares-tech-announced-partnership-with-media-net-f3ac85255924',
+  },
+  {
     src: '/images/media1.png',
     title: <FormattedMessage id="update.media.0.title" />,
     description: <FormattedMessage id="update.media.0.description" />,
@@ -14,29 +38,5 @@ export default [
     description: <FormattedMessage id="update.media.1.description" />,
     link:
       'https://medium.com/ares-tech/how-does-ares-tech-make-exchanging-erc20-token-with-erc721-asset-possible-a66952a84cb2',
-  },
-  {
-    src: '/images/media3.png',
-    title: <FormattedMessage id="update.media.2.title" />,
-    description: <FormattedMessage id="update.media.2.description" />,
-    link: 'https://www.youtube.com/channel/UCV_O9CKVKr4-KJVC-v8FcVw',
-  },
-  {
-    src: '/images/media4.png',
-    title: <FormattedMessage id="update.media.3.title" />,
-    description: <FormattedMessage id="update.media.3.description" />,
-    link: 'https://medium.com/ares-tech/international-investor-delegation-visited-ares-tech-fa09d3bc8c0f',
-  },
-  {
-    src: '/images/media5.png',
-    title: <FormattedMessage id="update.media.4.title" />,
-    description: <FormattedMessage id="update.media.4.description" />,
-    link: 'http://arestech.io/reg.php',
-  },
-  {
-    src: '/images/media6.png',
-    title: <FormattedMessage id="update.media.5.title" />,
-    description: <FormattedMessage id="update.media.5.description" />,
-    link: 'https://www.gruenderszene.de/allgemein/blockchain-social-gaming-ares-tech-2018-10642',
   },
 ]
