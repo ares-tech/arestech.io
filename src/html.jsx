f58e7aa77f75a3aa4ed72007d6c3160caca1a124
Fei Liu <liu.feiwood@gmail.com>
2018-10-12 22:41:59 +0200

    fix bottom share link (telegram, github etc.)

--- src/html.jsx
+++ src/html.jsx
@@ -40,12 +40,6 @@ export default class HTML extends React.Component {
           {this.props.headComponents}
           <link rel="shortcut icon" href={favicon} />
           {css}
-          <script
-            defer
-            src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"
-            integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe"
-            crossOrigin="anonymous"
-          />
         </head>
         <body>
           <div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
